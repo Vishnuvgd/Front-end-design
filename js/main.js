94e1998389209a1683cf76a81d54567039520dcf
vishnuvgd <vishnudaskallingal7@gmail.com>
2026-01-15 15:31:59 +0530

    add

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-    // $('.service-slider').slick({
-    //     slidesToShow: 4,
-    //     arrows: false,
-    //     dots: true,
-    //     responsive: [
-    //         { breakpoint: 992, settings: { slidesToShow: 2 } },
-    //         { breakpoint: 576, settings: { slidesToShow: 1 } }
-    //     ]
-    // });
+    $('.services-slider').slick({
+        slidesToShow: 3,
+        arrows: true,
+        dots: false,
+        responsive: [
+            { breakpoint: 992, settings: { slidesToShow: 2 } },
+            { breakpoint: 576, settings: { slidesToShow: 1 } }
+        ]
+    });
 
     $('.portfolio-slider').slick({
         slidesToShow: 3,
